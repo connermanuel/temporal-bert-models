f012f30b706ae783a8a3a75bf0de8d273088016f
connermanuel <manuel.conner.g@gmail.com>
2023-07-31 17:17:36 +0800

    implement seq classification

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name='tempo_models',
       version='0.1',
       description='Implementations of various temporal attention mechanisms.',
       packages=find_packages(),
+      package_dir={'':'src'}
      )
