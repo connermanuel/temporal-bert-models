42f4d4d4ceed9bd019c0429aaa54e555f480eb3b
connermanuel <manuel.conner.g@gmail.com>
2023-08-11 00:04:21 +0800

    reformat training and evaluation scripts

--- src/tempo_models/evaluate.py
+++ src/tempo_models/evaluate.py
@@ -4,12 +4,13 @@ Given a directory containing model checkpoints, evaluate all of those checkpoint
 """
 from tempo_models.models.bert.orthogonal_bert import BertForOrthogonalMaskedLM, BertForOrthogonalSequenceClassification
 from tempo_models.models.bert.tempo_bert import BertForTemporalMaskedLM, BertForTemporalSequenceClassification
+from tempo_models.utils.collator import CollatorCLS, CollatorMLM
+from tempo_models.utils.metrics import create_metric_func, mlm_metric_accuracy, mlm_metric_mrr, cls_metric_accuracy, cls_metric_per_class_f1, cls_metric_weighted_f1
 
-from torch import device as torch_device
 from transformers import BertForMaskedLM, AutoTokenizer, BertForSequenceClassification, TrainingArguments
 from datasets import load_from_disk
 
-from tempo_models.utils.utils import get_collator, evaluate_mlm, evaluate_span_accuracy, add_special_time_tokens, sort_by_timestamp, NonShuffledTrainer
+from tempo_models.utils.utils import evaluate_mlm, evaluate_span_accuracy, add_special_time_tokens, NonShuffledTrainer
 import os
 import logging
 import json
@@ -25,20 +26,21 @@ MODELS = {
     "bert_cls": BertForSequenceClassification
     }
 
-def fetch_model(model_architecture: str, checkpoint_path: str, task: str):
-    return MODELS[f"{model_architecture}_{task}"].from_pretrained(checkpoint_path)
+def fetch_model(model_architecture: str, checkpoint_dir: str, task: str):
+    return MODELS[f"{model_architecture}_{task}"].from_pretrained(checkpoint_dir)
 
 def evaluate(args):
     ### Fix kwargs, create directories, and setup logging
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     model_str = f"{args.model_architecture}"
     if args.model_architecture == "orthogonal":
         model_str = f"{args.model_architecture}_{args.alpha}"
-    if args.checkpoint_path is None and args.checkpoint_group_dir is None:
-        args.checkpoint_path = f"outputs/{model_str}"
+    if args.checkpoint_dir is None and args.checkpoint_group_dir is None:
+        args.checkpoint_dir = f"outputs/{model_str}"
     if args.results_dir is None:
         args.results_dir = f"results/{model_str}"
     
-    if args.checkpoint_path and not os.path.exists(args.checkpoint_path):
+    if args.checkpoint_dir and not os.path.exists(args.checkpoint_dir):
         raise ValueError("Checkpoint directory does not exist")
     elif args.checkpoint_group_dir and not os.path.exists(args.checkpoint_group_dir):
         raise ValueError("Checkpoint group directory does not exist")
@@ -61,12 +63,10 @@ def evaluate(args):
         bert_tokenizer.add_tokens(special_tokens)
     
     mask = not args.no_mask
-    if args.add_time_tokens == "string":
-        collator = get_collator(bert_tokenizer, do_masking=mask)
-    elif args.add_time_tokens == "special":
-        collator = get_collator(bert_tokenizer, n_tokens=1, do_masking=mask)
-    else:
-        collator = get_collator(bert_tokenizer, do_masking=mask)
+    if args.task == "mlm":
+        collator = CollatorMLM(bert_tokenizer)
+    elif args.task == "cls":
+        collator = CollatorCLS(bert_tokenizer)
 
     ### Load and process dataset
     logging.info(f"Loading dataset...")
@@ -82,7 +82,6 @@ def evaluate(args):
     
     logging.info(f"Processing the dataset")
     if args.process_dataset:
-        dataset = sort_by_timestamp(dataset)
         if args.add_time_tokens == "string":
             logging.info(f"Adding string time tokens")
             ## TODO
@@ -94,64 +93,53 @@ def evaluate(args):
         logging.info(f"Saving the dataset to {args.save_dataset}")
         dataset.save_to_disk(args.save_dataset)
 
-    if "word_ids" in dataset.features:
-        dataset = dataset.remove_columns("word_ids")
-    if "subreddit" in dataset.features:
-        dataset = dataset.remove_columns("subreddit") 
-    if args.model_architecture == "bert" and "timestamps" in dataset.features:
-        dataset = dataset.remove_columns("timestamps") 
     
-    ### Prepare evaluation setup
-    if args.no_cuda:
-        device = torch_device("cpu")
-    else:
-        device = torch_device("cuda") 
-    
-    if args.checkpoint_path:
-        model_dirs = [args.evaluate_path]
+    ### Prepare evaluation setup    
+    if args.checkpoint_dir:
+        model_dirs = [args.checkpoint_dir]
     elif args.checkpoint_group_dir:
-        model_dirs = [path for path in os.listdir(args.checkpoint_group_dir) if "checkpoint" in path]
+        model_dirs = [f"{args.checkpoint_group_dir}/{path}" for path in os.listdir(args.checkpoint_group_dir) if "checkpoint" in path]
     
     logging.info(f"Evaluating models...")
 
     eval_args = TrainingArguments(
-
+        output_dir=".",
+        per_device_eval_batch_size=args.batch_size,
+        remove_unused_columns=True,
+        fp16=args.use_fp16,
+        no_cuda=args.no_cuda,
+        eval_accumulation_steps=4,
     )
 
-    for dir in model_dirs:
+    metric_func = create_metric_func({})
+    if args.task == "mlm":
+        metric_func = create_metric_func({
+            "mrr": mlm_metric_mrr,
+            "accuracy": mlm_metric_accuracy
+        })
+    elif args.task == "cls":
+        metric_func = create_metric_func({
+            "accuracy": cls_metric_accuracy,
+            "per_class_f1": cls_metric_per_class_f1,
+            "weighted_f1": cls_metric_weighted_f1
+        })
+
+    dataset = dataset.remove_columns(["timestamps", "subreddit"])
+    results = {}
+    for dir in tqdm.tqdm(model_dirs):
         model = fetch_model(args.model_architecture, dir, args.task)
-        trainer = NonShuffledTrainer(
-            model=model,
-            eval_dataset=dataset,
-            compute_metrics=metric_func,
-            collator=collator
-        )
+        # trainer = NonShuffledTrainer(
+        #     model=model,
+        #     args=eval_args,
+        #     eval_dataset=dataset,
+        #     compute_metrics=metric_func,
+        #     data_collator=collator
+        # )
 
-    ### Evaluate models
-    def evaluate_path(checkpoint_path, architecture, f1, batch_size, results_dir):
-        model = fetch_model(architecture, checkpoint_path)
-        if f1:
-            result = evaluate_span_accuracy(model, dataset, collator, device, batch_size)
-            with open(f"{results_dir}/results.json", "w") as f:
-                json.dump({"accuracy": result}, f)
-        else:
-            result = evaluate_mlm(model, dataset, collator, device, batch_size)
-            for k, v in result.items():
-                results[k].append(v)
-            results['paths'].append(checkpoint_path)
-
-            with open(f"{results_dir}/results.json", "w") as f:
-                json.dump(results, f)
-    
-    if args.checkpoint_path:
-        evaluate_path(args.checkpoint_path, args.model_architecture, args.f1, args.batch_size, args.results_dir)
-    elif args.checkpoint_group_dir:
-        for checkpoint_path in tqdm.tqdm(sorted(os.listdir(args.checkpoint_group_dir))):
-            if checkpoint_path == "run.log":
-                continue
-            try:
-                full_checkpoint_path = f"{args.checkpoint_group_dir}/{checkpoint_path}"
-                evaluate_path(full_checkpoint_path, args.model_architecture, args.f1, args.batch_size, args.results_dir)
-            except OSError:
-                print(f"Could not evaluate {checkpoint_path}")
+        # model_results = trainer.evaluate()
+        model_results = evaluate_mlm(model, dataset, collator)
+        results[dir] = model_results
+        
+        with open(f"{args.results_dir}/results.json", "w") as f:
+            json.dump(results, f)
         
